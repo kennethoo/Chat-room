52ab6121ffa60dba19417ed2f78d3b49304581eb
kenneth <kcgandonou19@gmail.com>
2020-03-19 20:56:40 -0400

    make it better

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 
 
 
-const socket = io("http://localhost:3000/")
+const socket = io("https://text19-me.herokuapp.com")
 let list = document.getElementById("list")
 let closelist= document.querySelector(".closeslist")
 let people= document.getElementById("people")
